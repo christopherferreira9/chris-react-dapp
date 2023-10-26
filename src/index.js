00fa489ad0413b2a7304adbace0dfe8633af1a2b
Christopher Ferreira <christopher.ferreira@consensys.net>
2023-10-26 10:57:21 +0100

    Adds dapp

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetaMaskProvider } from '@metamask/sdk-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider debug={false} sdkOptions={{
+      logging:{
+          developerMode: false,
+        },
+        checkInstallationImmediately: false, // This will automatically connect to MetaMask on page load
+        i18nOptions: {
+          enabled: true,
+        },
+        dappMetadata: {
+          name: "Chris React DApp",
+          url: window.location.host,
+        }
+    }}>
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
